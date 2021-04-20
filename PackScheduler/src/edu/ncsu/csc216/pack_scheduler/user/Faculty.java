e0cbb542f13c47821bbaf8747cc4f15e4d99046f
cmhooks <cmhooks@ncsu.edu>
2021-04-19 22:34:49 -0400

    [debugging] fix pmd error

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Faculty.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Faculty.java
@@ -88,9 +88,7 @@ public class Faculty extends User {
 		if (getClass() != obj.getClass())
 			return false;
 		Faculty other = (Faculty) obj;
-		if (maxCourses != other.maxCourses)
-			return false;
-		return true;
+		return maxCourses == other.maxCourses;
 	}
 
 	/**
