175acb8f494d573f023a398792de6395150931ae
Daniel Nolting <dpnoltin@ncsu.edu>
2021-04-21 16:13:23 -0400

    [Implementation] Implemented LinkedRecursiveList

---
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedListRecursive.java
@@ -0,0 +1,220 @@
+package edu.ncsu.csc216.pack_scheduler.util;
+
+public class LinkedListRecursive<E> {
+	
+	private ListNode front;
+	
+	private int size;
+	
+	public LinkedListRecursive() {
+		front = null;
+		size = 0;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public boolean add(E element) {
+		if (element == null || contains(element)) {
+			throw new IllegalArgumentException();
+		}
+		if (isEmpty()) {
+			front = new ListNode(element, null);
+			size++;
+			return true;
+		} else {
+			return front.add(element);
+		}
+	}
+	 
+	public void add(int idx, E element) {
+		if (element == null) {
+			throw new NullPointerException();
+		}
+		if (contains(element)) {
+			throw new IllegalArgumentException();
+		}
+		
+		if (idx == 0) {
+			front = new ListNode(element, front);
+			size++;
+			return;
+		} 
+		
+		if (idx < 0 || idx > size) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		front.add(idx, element);
+		
+	}
+	
+	public E get(int idx) {
+		if (idx < 0 || idx >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		return front.get(idx);
+	}
+	
+	public boolean remove(E element) {
+		if (isEmpty()) {
+			return false;
+		} 
+		if (element == null) {
+			throw new NullPointerException();
+		}
+		if (front.data.equals(element)) {
+			front = front.next;
+			size--;
+			return true;
+		} else {
+			return front.remove(element);
+		}
+	}
+	
+	public E remove(int idx) {
+		if (isEmpty()) {
+			throw new IllegalArgumentException();
+		} 
+		if (idx < 0 || idx >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (idx == 0) {
+			E returnValue = front.data;
+			front = front.next;
+			size--;
+			return returnValue;
+		} else {
+			return front.remove(idx);
+		}
+	}
+	
+	public E set(int idx, E element) {
+		if (isEmpty()) {
+			throw new IllegalArgumentException();
+		} 
+		if (idx < 0 || idx >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		return front.set(idx, element);
+	}
+	
+	public boolean contains(E element) {
+		if (element == null) {
+			throw new NullPointerException();
+		}
+		if (isEmpty()) {
+			return false;
+		}
+		return front.contains(element);
+	}
+	/**
+	 * The ListNode class is an inner class of LinkedList. It constructs the Nodes
+	 * of the LinkedList.
+	 * 
+	 * @author Daniel Nolting
+	 * @author Calli Hooks
+	 * @author Owen Loker
+	 *
+	 */
+	private class ListNode {
+
+		/** The data in the node */
+		public E data;
+		/** The next node in the list */
+		public ListNode next;
+	
+	
+		/**
+		 * Constructs the ListNode object given the data for the node and the next node,
+
+		 * 
+		 * @param data the ListNode should contain
+		 * @param next the next node in the list
+		 */
+		public ListNode(E data, ListNode next) {
+			this.data = data;
+			this.next = next;
+		}
+		
+		
+		private boolean add(E element) {
+			if (next == null) {
+				front.next = new ListNode(element, null);
+				size++;
+				return true;
+			} else {
+				return next.add(element);
+			}
+		}
+		
+		private void add(int idx, E element) {
+			if (idx == 0) {
+				next = new ListNode(element, next);
+				size++;
+			} else if (next == null) {
+				throw new IndexOutOfBoundsException();
+			} else {
+				next.add(idx - 1, element);
+			}
+		}
+		
+		private E get(int idx) {
+			if (idx == 0) {
+				return data;
+			} else {
+				return next.get(idx - 1);
+			}
+		}
+		
+		private boolean remove(E element) {
+			if (next == null) {
+				return false;
+			} else if (next.data.equals(element)) {
+				next = next.next;
+				size--;
+				return true;
+			} else {
+				return next.remove(element);
+			}
+		}
+		
+		private E remove(int idx) {
+			if (idx == 1) {
+				E returnValue = next.data;
+				next = next.next;
+				size--;
+				return returnValue;
+			} else {
+				return next.remove(idx - 1);
+			}
+		}
+		
+		private E set(int idx, E element) {
+			if (idx == 0) {
+				E returnValue = data;
+				data = element;
+				return returnValue;
+			} else {
+				return next.set(idx - 1, element);
+			}
+		}
+		
+		private boolean contains(E element) {
+			if (data == element) {
+				return true;
+			} else if (next == null) {
+				return false;
+			} else {
+				return next.contains(element);
+			}
+		}
+	}
+
+}
---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/LinkedListRecursiveTest.java
@@ -0,0 +1,133 @@
+package edu.ncsu.csc216.pack_scheduler.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LinkedListRecursiveTest {
+
+	/**
+	 * Tests the constructor
+	 */
+	@Test
+	public void testLinkedList() {
+		LinkedListRecursive<String> list = new LinkedListRecursive<String>();
+		assertEquals(0, list.size());
+	}
+
+	/**
+	 * Test add function
+	 */
+	@Test
+	public void testAddIdx() {
+		LinkedListRecursive<String> list = new LinkedListRecursive<String>();
+
+		list.add(0, "apple");
+		assertEquals(1, list.size());
+		assertEquals("apple", list.get(0));
+
+		list.add(1, "banana");
+		assertEquals(2, list.size());
+		assertEquals("banana", list.get(1));
+
+		list.add(2, "cat");
+		assertEquals(3, list.size());
+		assertEquals("cat", list.get(2));
+
+		list.add(3, "dragonfruit");
+		assertEquals(4, list.size());
+		assertEquals("dragonfruit", list.get(3));
+
+		list.add(4, "grapefruit");
+		assertEquals(5, list.size());
+		assertEquals("grapefruit", list.get(4));
+
+		list.add(5, "honeydew");
+		assertEquals(6, list.size());
+		assertEquals("honeydew", list.get(5));
+
+		list.add(6, "juniper berry");
+		assertEquals(7, list.size());
+		assertEquals("juniper berry", list.get(6));
+
+		list.add(7, "kiwi");
+		assertEquals(8, list.size());
+		assertEquals("kiwi", list.get(7));
+
+		list.add(8, "lemon");
+		assertEquals(9, list.size());
+		assertEquals("lemon", list.get(8));
+
+		list.add(9, "mango");
+		assertEquals(10, list.size());
+		assertEquals("mango", list.get(9));
+
+		list.add(10, "orange");
+		assertEquals(11, list.size());
+		assertEquals("orange", list.get(10));
+
+		list.add(11, "pomegranite");
+		assertEquals(12, list.size());
+		assertEquals("pomegranite", list.get(11));
+
+		// Try to add null element
+		try {
+			list.add(11, null);
+			fail();
+		} catch (NullPointerException e) {
+			assertEquals("pomegranite", list.get(11));
+
+		}
+
+	}
+
+	/**
+	 * Tests remove function
+	 */
+	@Test
+	public void testRemove() {
+		LinkedListRecursive<String> list = new LinkedListRecursive<String>();
+		list.add(0, "apple");
+		list.add(1, "banana");
+		list.add(2, "carrot");
+		list.add(3, "dog");
+
+		assertEquals(4, list.size());
+
+		list.remove(2);
+		assertEquals(3, list.size());
+		assertEquals("apple", list.get(0));
+		assertEquals("banana", list.get(1));
+		assertEquals("dog", list.get(2));
+
+		list.remove(0);
+		assertEquals(2, list.size());
+		assertEquals("banana", list.get(0));
+		assertEquals("dog", list.get(1));
+
+		list.remove(1);
+		assertEquals(1, list.size());
+		assertEquals("banana", list.get(0));
+
+		list.remove(0);
+		assertEquals(0, list.size());
+	}
+
+	/**
+	 * Tests set function
+	 */
+	@Test
+	public void testSet() {
+		LinkedListRecursive<String> list = new LinkedListRecursive<String>();
+		list.add(0, "apple");
+		list.add(1, "banana");
+		list.add(2, "carrot");
+		list.add(3, "dog");
+
+		assertEquals(4, list.size());
+
+		list.set(3, "cat");
+		assertEquals("cat", list.get(3));
+	}
+
+}
